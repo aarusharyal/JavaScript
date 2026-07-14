0bae78fc7a3c36a43b5b1af3b55879efe244160d
aarusharyal <aryalaarush203@gmail.com>
2026-07-14 18:34:12 +0545

    Game For Heads Or Tails

--- headsOrTails/app.js
+++ headsOrTails/app.js
@@ -3,3 +3,23 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const options =['heads', 'tails'];
+
+rl.question('Do you wish to start :(y/n) ', (choice)
+{
+    switch (choice.toLowerCase())
+    {
+        case 'yes':
+            case 'y':
+                case '1':
+                    play();
+                    break;
+        case 'no':
+            case 'n':
+                case '2':
+                    rl.close();
+                    break;
+    }
+});
+
