3ea9fcbb0c6e8e535c71d06164e1db2bbe7f13d4
aarusharyal <aryalaarush203@gmail.com>
2026-07-14 18:49:51 +0545

    Game Of Heads Or Tails

--- headsOrTails/app.js
+++ headsOrTails/app.js
@@ -4,22 +4,26 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const options =['heads', 'tails'];
+const options = ["Heads", "Tails"];
 
-rl.question('Do you wish to start :(y/n) ', (choice)
-{
-    switch (choice.toLowerCase())
-    {
-        case 'yes':
-            case 'y':
-                case '1':
-                    play();
-                    break;
-        case 'no':
-            case 'n':
-                case '2':
-                    rl.close();
-                    break;
-    }
+rl.question("Do you wish to start :(y/n) ", (choice) => {
+  switch (choice.toLowerCase()) {
+    case "yes":
+    case "y":
+    case "1":
+      play();
+      break;
+    case "no":
+    case "n":
+    case "2":
+      rl.close();
+      break;
+  }
 });
 
+function play() {
+  const randomIndex = Math.floor(Math.random() * options.length);
+  const randomChoice = options[randomIndex];
+  console.log(randomChoice);
+  rl.close();
+}
