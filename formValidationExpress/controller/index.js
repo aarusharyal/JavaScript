4a802438dc4291de141208248486077efbd3a9bb
aarusharyal <aryalaarush203@gmail.com>
2026-07-23 22:32:29 +0545

    Did Some Changes

--- formValidationExpress/controller/index.js
+++ formValidationExpress/controller/index.js
@@ -3,11 +3,18 @@ import path from "path";
 
 const app = express();
 
-app.use(express.static("../view/CSS"));
-app.use(express.static("../view/JS"));
+app.use(express.static("./view/CSS"));
+app.use(express.static("./view/JS"));
 
-app.use((req, res) => {
-  res.status(404).render("Error", { message: "Page Not Found" });
+app.get("/", (req, res) => {
+  const absoluteValue = path.resolve("./view/Html/login.html");
+  res.sendFile(absoluteValue);
+  console.log("Dashboard Loaded Succesfully");
+});
+app.get("/register", (req, res) => {
+  const absoluteValue = path.resolve("./view/Html/register.html");
+  res.sendFile(absoluteValue);
+  console.log("Register Page loaded succesfully");
 });
 const PORT = 3000;
 app.listen(PORT, () => {
